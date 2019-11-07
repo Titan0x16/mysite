8fe36fb0665be96491608408bb6962f55c2d78e9
Prime Ape <primeape@vrotonog.com>
2019-11-07 19:30:54 +0300

    22

--- flask_app.py
+++ flask_app.py
@@ -10,7 +10,7 @@ sslify = SSLify(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Git21!'
+    return 'Hello from Git22!'
 
 @app.route('/update_server', methods=['POST'])
 def webhook():
