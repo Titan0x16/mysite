be4c27e7af90db8f90064148d6e4ba01a1735fda
Prime Ape <primeape@vrotonog.com>
2019-11-07 23:09:56 +0300

    47

--- flask_app.py
+++ flask_app.py
@@ -17,15 +17,15 @@ sslify = SSLify(app)
 def webhook():
     if request.method == 'POST':
     	filepath = os.path.abspath(os.path.dirname(__file__))
-        repo = git.Repo(filepath)
-        origin = repo.remotes.origin
+    	repo = git.Repo(filepath)
+    	origin = repo.remotes.origin
 
-        origin.pull()
-        return 'Updated PythonAnywhere successfully', 200
+    	origin.pull()
+    	return 'Updated PythonAnywhere successfully', 200
     else:
     	path = os.getcwd()
     	path2 = os.path.abspath(os.path.dirname(__file__))
-    	return f'Hello from Git46! {path} {path2}'
+    	return f'Hello from Git47! {path} {path2}'
 
 if __name__ == '__main__':
     app.run()
