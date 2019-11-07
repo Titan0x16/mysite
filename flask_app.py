12d9218690a4832ce41f7f5e0a3448b33f048326
Prime Ape <primeape@vrotonog.com>
2019-11-07 14:45:38 +0300

    git webhook3

--- flask_app.py
+++ flask_app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Git3!'
+    return 'Hello from Git4!'
 
 @app.route('/update_server', methods=['POST'])
 def webhook():
