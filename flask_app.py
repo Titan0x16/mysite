4bc6cf7fafac2f5aa2ab54a32a2d6f7731e0574c
Prime Ape <primeape@vrotonog.com>
2019-11-07 18:28:32 +0300

    10

--- flask_app.py
+++ flask_app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Git9!'
+    return 'Hello from Git10!'
 
 @app.route('/update_server', methods=['POST'])
 def webhook():
