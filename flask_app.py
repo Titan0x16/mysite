e0ec6cb80d752da364c183f6d4de086dda2d498a
Prime Ape <primeape@vrotonog.com>
2019-11-07 22:43:26 +0300

    39

--- flask_app.py
+++ flask_app.py
@@ -22,7 +22,7 @@ def webhook():
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
     else:
-        return 'Hello from Git38!'
+        return 'Hello from Git39!'
 
 if __name__ == '__main__':
     app.run()
